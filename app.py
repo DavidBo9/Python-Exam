3ec9dfd62ec6fe9c212bc2b9a468056887efb86c
David Bojalil <194228@iberopuebla.mx>
2024-04-03 08:39:18 -0600

    Created python exam app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
